a9c2aeb2f05d3d68f2fe17bdfdfd35fd29624bf4
Cyperfroz00 <pen_ponlok@pp.bbu.edu.kh>
2023-08-14 18:30:07 +0700

    1st

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatefulWidget {
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+    );
   }
 }
