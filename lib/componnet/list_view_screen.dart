7e226cee26ea5c7e45940a88403249c64d9bf38f
Cyperfroz00 <pen_ponlok@pp.bbu.edu.kh>
2023-08-14 19:04:23 +0700

    2nd update list view

--- lib/componnet/list_view_screen.dart
+++ lib/componnet/list_view_screen.dart
@@ -20,8 +20,37 @@ class _ListviewscreenState extends State<Listviewscreen> {
         child: ListView(
           children: [
             Container(
+              margin: EdgeInsets.all(5),
+              color: Colors.pinkAccent,
               child: ListTile(
               leading: Icon(Icons.home),
+                title: Text('Home'),
+                trailing: Icon(Icons.more_horiz),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              color: Colors.pinkAccent,
+              child: ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home'),
+                trailing: Icon(Icons.more_horiz),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              color: Colors.pinkAccent,
+              child: ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home'),
+                trailing: Icon(Icons.more_horiz),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(5),
+              color: Colors.pinkAccent,
+              child: ListTile(
+                leading: Icon(Icons.home),
                 title: Text('Home'),
                 trailing: Icon(Icons.more_horiz),
               ),
