354db78cd1da27d559d4471b61750d770c6c690c
Cyperfroz00 <pen_ponlok@pp.bbu.edu.kh>
2023-08-15 13:07:29 +0700

    7th update drawer with profile

--- lib/componnet/list_view_screen.dart
+++ lib/componnet/list_view_screen.dart
@@ -17,50 +17,121 @@ class _ListviewscreenState extends State<Listviewscreen> {
         child: Container(
           height: 150,
           width: 150,
-          child: ListView(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(250),
+          child: Container(
+            child: ListView(
+              children: [
+                Center(
+                  child: Container(
+
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(250),
+                        ),
+                        color: Colors.pinkAccent),
+                    width: 150,
+                    height: 150,
+                    margin: EdgeInsets.all(5),
+                    // width: 100,
+                    // height: 100,
+                    child: Image.asset(
+                      'assets/images/ceo.jpg'
                     ),
-                    color: Colors.pinkAccent),
-                width: 150,
-                height: 150,
-                margin: EdgeInsets.all(5),
-                // width: 100,
-                // height: 100,
-                child: ListTile(),
-              ),
-              SizedBox(),
-              Container(
-                margin: EdgeInsets.all(5),
-                color: Colors.yellow[500],
-                child: ListTile(
-                  leading: Icon(Icons.map),
-                  title: Text('Map'),
-                  trailing: Icon(Icons.more_horiz),
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.all(5),
-                color: Colors.pinkAccent,
-                child: ListTile(
-                  leading: Icon(Icons.phone_android),
-                  title: Text('Phone'),
-                  trailing: Icon(Icons.more_horiz),
+                SizedBox(),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.yellow[500],
+                  child: ListTile(
+                    //leading: Icon(Icons.map),
+                    title: Text('Map'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.all(5),
-                color: Colors.green,
-                child: ListTile(
-                  leading: Icon(Icons.call),
-                  title: Text('Call'),
-                  trailing: Icon(Icons.more_horiz),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  height: 150,
+                  color: Colors.blue,
+                  child: ListTile(
+                    ///leading: Icon(Icons.phone_android),
+                    title: Text('Phone'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: ListTile(
+                    //leading: Icon(Icons.call),
+                    title: Text('Call'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: ListTile(
+                    //leading: Icon(Icons.call),
+                    title: Text('Call'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: ListTile(
+                    //leading: Icon(Icons.call),
+                    title: Text('Call'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: ListTile(
+                    //leading: Icon(Icons.call),
+                    title: Text('Call'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: ListTile(
+                    //leading: Icon(Icons.call),
+                    title: Text('Call'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: ListTile(
+                    //leading: Icon(Icons.call),
+                    title: Text('Call'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: ListTile(
+                    //leading: Icon(Icons.call),
+                    title: Text('Call'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: ListTile(
+                    //leading: Icon(Icons.call),
+                    title: Text('Call'),
+                    //trailing: Icon(Icons.more_horiz),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
